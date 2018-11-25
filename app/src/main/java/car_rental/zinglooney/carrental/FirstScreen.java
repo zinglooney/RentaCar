6c4e19967a0d04d8c2425c1172bbdbc86a1fcbcd
zinglooney <zinglooney@hotmail.com>
2018-11-25 03:48:41 +0200

    Betta 1

--- app/src/main/java/car_rental/zinglooney/carrental/FirstScreen.java
+++ app/src/main/java/car_rental/zinglooney/carrental/FirstScreen.java
@@ -1,32 +1,38 @@
 package car_rental.zinglooney.carrental;
 
+import android.app.DatePickerDialog;
 import android.app.ListActivity;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import java.util.Calendar;
 
-import java.util.List;
+
 
 public class FirstScreen extends ListActivity {
 
-    private TextView msg;
+    private TextView lopt;
+    private Button btndp;
+    private Button btntp;
 
-
-    /*msg = (TextView) findViewByid(R.id.listMessage);
-        @Override
-        protected void onListItemClick(ListView l, View v, int position, long id) {
-            super.onListItemClick(l, v, position, id);
-        }*/
 
 
     private String[] Car_type = {
             "Class A",
             "Class B",
-            "Class C"
+            "Class C",
+            "Class D",
+            "Class E"
     };
 
 
@@ -35,18 +41,89 @@ public class FirstScreen extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firstscreen);
 
+
+        lopt = (TextView)findViewById(R.id.listOption);
         setListAdapter(new ArrayAdapter <>(this,android.R.layout.simple_list_item_1, Car_type));
 
-        //msg.setText("Click Here" +getSelectedItemPosition());
+        btndp = findViewById(R.id.btnDatePicker);
+        btntp  = findViewById(R.id.btnTimePicker);
+
+        btndp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar c = Calendar.getInstance();
+                int day = c.get(Calendar.DAY_OF_MONTH);
+                int month = c.get(Calendar.MONTH);
+                int year = c.get((Calendar.YEAR));
+                int hour = c.get(Calendar.HOUR_OF_DAY);
+                int minute = c.get(Calendar.MINUTE);
+
+                DatePickerDialog dpd = new DatePickerDialog(FirstScreen.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datepicker, int mYear, int mMonth, int mDay) {
+                        btndp.setText(mDay + "/" + (mMonth+1) + "/" + mYear);
+                    }
+                }, day, month, year);
+                TimePickerDialog tpd = new TimePickerDialog(FirstScreen.this,new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timepicker, int mH, int mM) {
+                        btntp.setText(mH + ":" + mM);
+                    }
+                }, hour, minute, true);
+               tpd.show();
+               dpd.show();
+
+
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        lopt.setText(((TextView)v).getText());
     }
 
     public void saveButton(View view) {
 
+
         EditText cn = findViewById(R.id.customer_name);
-        String message = cn.getText().toString();
+        String cncnt = cn.getText().toString();
+
+        Button btnvd = findViewById(R.id.btnDatePicker);
+        String btnvdcnt = btnvd.getText().toString();
+
+        Button btnvt = findViewById(R.id.btnTimePicker);
+        String btnvtcnt = btnvt.getText().toString();
+
+        TextView ct = findViewById(R.id.listOption);
+        String ctcnt = ct.getText().toString();
+
+        EditText dr = findViewById(R.id.duration);
+        String drcnt = dr.getText().toString();
+
+
+        RadioGroup rgplro = (RadioGroup) findViewById(R.id.lro);
+        int radioButtonIDlro = rgplro.getCheckedRadioButtonId();
+        RadioButton rbtnlro = (RadioButton) rgplro.findViewById(radioButtonIDlro);
+        String lrocnt = (String) rbtnlro.getText();
+
+        RadioGroup rgpro = (RadioGroup) findViewById(R.id.ro);
+        int radioButtonIDro = rgpro.getCheckedRadioButtonId();
+        RadioButton rbtnro = (RadioButton) rgpro.findViewById(radioButtonIDro);
+        String rocnt = (String) rbtnro.getText();
+
 
         Intent intent = new Intent(this,LastScreen.class);
-        intent.putExtra("EXTRA_MESSAGE",message);
+        intent.putExtra("CUSTOMER_NAME",cncnt);
+        intent.putExtra("DATE",btnvdcnt);
+        intent.putExtra("TIME",btnvtcnt);
+        intent.putExtra("CAR_TYPE", ctcnt);
+        intent.putExtra("DURATION", drcnt);
+        intent.putExtra("LOCATION_OPTION", lrocnt);
+        intent.putExtra("RENT_OPTION", rocnt);
         startActivity(intent);
     }
 }
--- app/src/main/java/car_rental/zinglooney/carrental/LastScreen.java
+++ app/src/main/java/car_rental/zinglooney/carrental/LastScreen.java
@@ -18,9 +18,37 @@ public class LastScreen extends AppCompatActivity {
 
         Intent intent = getIntent();
 
-        String message = intent.getStringExtra("EXTRA_MESSAGE");
+        String cncnt = intent.getStringExtra("CUSTOMER_NAME");
         TextView tv_cn = findViewById(R.id.tv_customer_name);
-        tv_cn.setText(message);
+        tv_cn.setText(cncnt);
 
-     }
+        String btnvdcnt = intent.getStringExtra("DATE");
+        TextView tv_btnvd = findViewById(R.id.tv_date);
+        tv_btnvd.setText(btnvdcnt);
+
+        String btnvtcnt = intent.getStringExtra("TIME");
+        TextView tv_btnvt = findViewById(R.id.tv_time);
+        tv_btnvt.setText(btnvtcnt);
+
+        String ctcnt = intent.getStringExtra("CAR_TYPE");
+        TextView tv_ct = findViewById(R.id.tv_car_type);
+        tv_ct.setText(ctcnt);
+
+        String drcnt = intent.getStringExtra("DURATION");
+        TextView tv_dr = findViewById(R.id.tv_duration);
+        tv_dr.setText(drcnt);
+
+        String lrocnt = intent.getStringExtra("LOCATION_OPTION");
+        TextView tv_lro = findViewById(R.id.tv_location_receipt_options);
+        tv_lro.setText(lrocnt);
+
+        String rocnt = intent.getStringExtra("RENT_OPTION");
+        TextView tv_ro = findViewById(R.id.tv_rent_options);
+        tv_ro.setText(rocnt);
+
+
+
+
+
+    }
 }
